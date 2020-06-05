e7be21a2364a1b78a39a2a6d1eb16086dd4e8ba0
Joanna Preston <joannappreston@gmail.com>
2020-06-05 09:46:21 -0400

    changed config.json to config.js and set up environement cariables. added schema file for creating database

---
+++ config/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+module.exports = {
+    "development": {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE,
+        "host": process.env.DB_HOST,
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
+        "use_env_variable": "JAWSDB_URL",
+        "dialect": "mysql"
+    }
+};
