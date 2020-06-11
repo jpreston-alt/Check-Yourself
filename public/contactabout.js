a5d4e77e5f718ce4777cab7275c4c37cf6a80596
Christina Ausley <christinamausley@gmail.com>
2020-06-11 15:18:19 -0700

    initial commit

---
+++ public/contactabout.js
@@ -0,0 +1,9 @@
+// object-fit polyfill run
+objectFitImages();
+
+/* init Jarallax */
+jarallax(document.querySelectorAll('.jarallax'));
+
+jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+    keepImg: true,
+});
