6c95d530578b91ed00cf9fba8788980c93e57a08
Christina Ausley <christinamausley@gmail.com>
2020-06-11 21:29:46 -0700

    added footers, cleaned up code

--- public/contactabout.js
+++
@@ -1,9 +0,0 @@
-// object-fit polyfill run
-objectFitImages();
-
-/* init Jarallax */
-jarallax(document.querySelectorAll('.jarallax'));
-
-jarallax(document.querySelectorAll('.jarallax-keep-img'), {
-    keepImg: true,
-});
