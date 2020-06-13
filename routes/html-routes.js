06105466a8d442e3168a26f88b2224d639cafb2a
Daniel Luke Tanoeihusada <danielluke08@gmail.com>
2020-06-12 18:55:51 -0700

    changed routes from member to home page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,16 +10,16 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/incomes");
       console.log(req.user);
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/incomes");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
