01133903727e552d1f28f0351c4503fbc6938d7d
Joanna Preston <joannappreston@gmail.com>
2020-06-16 21:19:05 -0700

    changed color palette

--- public/js/home.js
+++ public/js/home.js
@@ -111,7 +111,7 @@ $(document).ready(() => {
           {
             label: "Categories",
             data: dataArr,
-            backgroundColor: ["#07456f", "#009f9d", "#cdffeb", "#0f0a3c"],
+            backgroundColor: ["#6a2c70", "#f08a5d", "#b83b5e"],
             borderWidth: 1,
             borderColor: "white",
             hoverBorderWidth: 3
@@ -120,7 +120,7 @@ $(document).ready(() => {
       },
       options: {
         title: {
-          display: true,
+          display: false,
           text: "Suggested Monthly Spending",
           fontSize: 25
         },
