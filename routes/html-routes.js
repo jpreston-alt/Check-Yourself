a5e7b54db786eb69e3a82a977e692d9e5b804c24
Joanna Preston <joannappreston@gmail.com>
2020-06-16 19:55:03 -0700

    deleted files we didn't need

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -25,11 +25,6 @@ module.exports = function(app) {
   });
 
   // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
-
   // budget route loads home.html page
   app.get("/home", isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, "../public/home.html"));
